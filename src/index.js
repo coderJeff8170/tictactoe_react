7c78d40428e7cdc74d5221f07a7529f701c4ea8a
Jeff Andrews <babajeff8170@gmail.com>
2022-06-12 19:09:55 -0400

    adds calculate winner method and updates status accordingly

--- src/index.js
+++ src/index.js
@@ -37,8 +37,9 @@ const Square = (props) => {
     }
 
     handleClick(i) {
-        if(this.state.squares[i]) return;
+        
         const squares = this.state.squares.slice();
+        if(calculateWinner(squares) || squares[i]) return;
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             squares: squares,
@@ -47,7 +48,14 @@ const Square = (props) => {
     }
   
     render() {
-      const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+      let status;
+      const winner = calculateWinner(this.state.squares)
+      if(winner) {
+        status = `${winner} has won!`;
+      } else {
+        status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+      }
+      
   
       return (
         <div>
@@ -86,6 +94,26 @@ const Square = (props) => {
         </div>
       );
     }
+  }
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
   }
   
   // ========================================
