ed59dc23014c169d9de9eec5aee2e9a44c8ae20f
Jeff Andrews <babajeff8170@gmail.com>
2022-06-14 16:47:49 -0400

    adds funtionality to jumpTo method  and finishes game

--- src/index.js
+++ src/index.js
@@ -87,12 +87,13 @@ const Square = (props) => {
               history: [{
                   squares: Array(9).fill(null)
               }],
+              stepNumber: 0,
               xIsNext: true
           }
       }
 
       handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length-1];
         
         const squares = current.squares.slice();
@@ -100,20 +101,30 @@ const Square = (props) => {
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             history: history.concat([{ squares: squares}]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext
         })
     }
 
+    jumpTo(step) {
+      this.setState({
+        stepNumber: step,
+        xIsNext: (step % 2) === 0
+      })
+    }
+
     render() {
       const history = this.state.history;
-      const current = history[history.length-1];
+      const current = history[this.state.stepNumber];
       const priorMoves = history.map((step, move) => {
           const desc = move ? 
           `Go to move ${move}` :
           `Go to game start`
-        return <li>
+        return (
+        <li key={move}>
             <button onClick={() => this.jumpTo(move)}>{desc}</button>
         </li>
+        );
     })
       let status;
       const winner = calculateWinner(current.squares);
